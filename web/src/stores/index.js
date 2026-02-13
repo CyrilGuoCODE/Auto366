2df55873caa1732b799bd26fd87f4e9c90df3744
cyrilguocode <cyril_0614@hotmail.com>
2026-02-13 09:48:59 +0800

    删除web代码(仓库转移)

--- web/src/stores/index.js
+++
@@ -1,5 +0,0 @@
-// Export all stores for easy importing
-export { useAuthStore } from './auth.js'
-export { useRulesetsStore } from './rulesets.js'
-export { useUIStore } from './ui.js'
-export { useCounterStore } from './counter.js'
